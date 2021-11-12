ea155a48100b0058869381871f40b6387834d703
Tim Quimbo <tquimbo1@gmail.com>
2021-11-11 22:20:48 -0500

    commit tree 6259a5687604c781787dea9f7b87ed9b1da17aad

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(people, occasion) {
+    let arr = [];
+  
+    for (let i = 0; i < people.length; i++) {
+      arr.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return arr;
+  }
+  
+
+
+
+
+function countDown(number) {
+    while(number >= 0) {
+      console.log(number--);
+    }
+  }
+  
